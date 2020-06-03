9585570f1607398aa1cf54d25bde3b05f4db6f8d
Mustafa Ozsakalli <mustafa@digitoygames.com>
2020-06-03 11:37:29 +0300

    no message

---
+++ jvmtest/src/main/java/digiplay/render/RenderState.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digiplay.render;
+
+/**
+ *
+ * @author mustafa
+ */
+public class RenderState {
+    
+    public boolean depthWrite;
+    public int depthTest;
+ 
+    public int blendSource;
+    public int blendDestination;
+    public int 
+}
